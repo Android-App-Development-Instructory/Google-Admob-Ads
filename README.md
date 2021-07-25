17e2a6b1da8048f57327b97ddd7d5be41e8d47ee
User <https://github.com/karno786/Sqlite_git_test>
2021-07-25 17:28:58 +0600

    Google Admob Ads

---
+++ README.md
@@ -0,0 +1,36 @@
+# Google Admob Ads
+
+This is a practice project for implement Google Admob Ads. Here I use latest SDK of Google Ads 20.0.+ . In this project I have implement Banner , Interstitial and Reward video Ads.
+
+# Screenshots:
+
+## 1. Banner Ads:
+<img src="https://github.com/karno786/Google-Admob-Ads/blob/master/screenshots/Banner_Ads.jpg" alt="Google Admob Ads Banner Screenshot" width="350" title="Google Admob Ads Banner" >
+
+
+
+## 2. Interstitial Ads Screen:
+<img src="https://github.com/karno786/Google-Admob-Ads/blob/master/screenshots/Interstitial_Ads.jpg" alt="Google Admob Ads Interstitial Screenshot" width="350" title="Google Admob Ads Interstitial" >
+
+
+
+## 3. Reward Video Ads Screen:
+<img src="https://github.com/karno786/Google-Admob-Ads/blob/master/screenshots/Reward_Ads.jpg" alt="Google Admob Ads Reward Screenshot" width="350" title="Google Admob Ads Reward" >
+
+
+
+### Disclaimer
+This project is made only for educational purpose. Anyone can use it but the risk has to be taken by the user.
+for any inquiry please contact me.
+
+### Repository Owner Info
+
+__Md. Al-Amin__ <br>
+__Email :__ [ alamin.karno@outlook.com ](mailto:alamin.karno@outlook.com) <br>
+__Github :__ [Md. Al-Amin](https://github.com/karno786)<br>
+__Facebook :__ [মোঃ আল-আমিন খন্দকার কর্ণ](https://facebook.com/alamin.kanro786) <br>
+__Linkedin :__ [Md. Alamin Karno](https://www.linkedin.com/in/alaminkarno/)
+<br>
+<br>
+
+
---
+++ app/src/main/java/com/alaminkarno/googleadmobads/MainActivity.java
@@ -0,0 +1,129 @@
+package com.alaminkarno.googleadmobads;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+    private RewardedAd mRewardedAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+       banner_ads();
+
+       interstitial_ads();
+
+       reward_ads();
+
+    }
+
+    private void reward_ads() {
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917",
+                adRequest, new RewardedAdLoadCallback() {
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error.
+                        Log.d("TAG", loadAdError.getMessage());
+                        mRewardedAd = null;
+                    }
+
+                    @Override
+                    public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                        mRewardedAd = rewardedAd;
+                        Log.d("TAG", "Ad was loaded.");
+                    }
+                });
+    }
+
+    private void interstitial_ads() {
+
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        mInterstitialAd = interstitialAd;
+                        Log.i("TAG", "onAdLoaded");
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i("TAG", loadAdError.getMessage());
+                        mInterstitialAd = null;
+                    }
+                });
+
+    }
+
+    private void banner_ads() {
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+
+    public void interstitialAdsButton(View view) {
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(MainActivity.this);
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.");
+        }
+    }
+
+    public void rewardAdsButton(View view) {
+
+        if (mRewardedAd != null) {
+            Activity activityContext = MainActivity.this;
+            mRewardedAd.show(activityContext, new OnUserEarnedRewardListener() {
+                @Override
+                public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                    // Handle the reward.
+                    Log.d("TAG", "The user earned the reward.");
+                    int rewardAmount = rewardItem.getAmount();
+                    String rewardType = rewardItem.getType();
+
+                    Toast.makeText(activityContext, "Amount: "+rewardAmount+"\nType: "+rewardType, Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Log.d("TAG", "The rewarded ad wasn't ready yet.");
+        }
+    }
+}
